078197a32a1bbb3abed96a9c4fe0b796b7f201d2
aditya bhojane <adityabhojane66@gmail.com>
2024-08-26 01:48:49 +0530

    FIX: add readable store name for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Store from "./context/store.jsx";
+import QuizProvider from "./context/QuizProvider.jsx";
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Store>
+    <QuizProvider>
       <App />
-    </Store>
+    </QuizProvider>
   </StrictMode>
 );
